674f2d496bde08c81ed944f8a36aec5da63a5935
Princess Irabor <irabor.princess1@gmail.com>
2019-06-25 23:25:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  this_hash={}
   this_hash[key] = value
   this_hash
 end
